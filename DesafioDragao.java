6a5c2cb7212029c560fb6d340ac7ca04c7887466
Marco Fernandes <109549998+MarcoAFSJ@users.noreply.github.com>
2023-02-22 21:44:07 -0300

    Update DesafioDragao.java

--- DesafioDragao.java
+++ DesafioDragao.java
@@ -3,7 +3,7 @@
 // - System.out.println:.imprime um texto de Saída (Output) e pulando uma linha.  
 
 import java.util.*;
-public class Solution{ 
+public class DesafioDragao{ 
     
    public static void main(String[] args){
             int casos, poderDeLuta;
@@ -15,7 +15,7 @@ public class Solution{
             for(int i = 0; i < casos; i++){
                 poderDeLuta = ler.nextInt();
 
-//TODO: Implemente a condição adequada para a impressão dos textos conforme o solicitado no desafio:
+                //TODO: Implemente a condição adequada para a impressão dos textos conforme o solicitado no desafio:
     
                 if(poderDeLuta <= 8000){
                     System.out.println("Inseto!");
