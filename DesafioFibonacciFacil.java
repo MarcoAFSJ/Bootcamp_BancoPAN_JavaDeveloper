a2f16dd359910cb51bd6b0127d23cfc8b89ca252
Marco Fernandes <109549998+MarcoAFSJ@users.noreply.github.com>
2023-02-22 21:48:49 -0300

    Update DesafioFibonacciFacil.java

--- DesafioFibonacciFacil.java
+++ DesafioFibonacciFacil.java
@@ -5,7 +5,7 @@
 import java.io.IOException;
 import java.util.Scanner;
 
-public class Teste {
+public class DesafioFibonacciFacil {
 
     public static void main(String[] args) throws IOException {
         Scanner leitor = new Scanner(System.in);
